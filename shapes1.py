42923b66db87ef4d0b19f59b959195571849f0dc
anweenie <awburns01@gmail.com>
2017-07-18 15:59:30 -0400

    Create shapes1.py

---
+++ shapes1.py
@@ -0,0 +1,41 @@
+from turtle import *
+import math
+
+# Name your Turtle.
+t = Turtle()
+
+# Set Up your screen and starting position.
+setup(600,400)
+#x_pos = -250
+#y_pos = -150
+#t.setposition(x_pos, y_pos)
+
+### Write your code below:
+pendown()
+pencolor("goldenrod")
+forward(90)
+left(60)
+back(90)
+left(60)
+forward(90)
+pendown()
+
+pendown()
+left(45)
+forward(90)
+left(45)
+forward(90)
+left(45)
+forward(90)
+left(45)
+forward(90)
+left(45)
+forward(90)
+left(45)
+forward(90)
+left(45)
+forward(90)
+left(45)
+forward(90)
+# Close window on click.
+exitonclick()
