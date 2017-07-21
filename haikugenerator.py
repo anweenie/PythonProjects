e783e118a4fe974c0638a959c6a5b64c022a5bd3
anweenie <awburns01@gmail.com>
2017-07-21 14:29:35 -0400

    Create haikugenerator.py

---
+++ haikugenerator.py
@@ -0,0 +1,7 @@
+import random
+lineOne = ['Hey I just met you', 'First I was afraid', 'Staring at the sun', 'Where do you belong?', 'Thick blanket of snow', 'Light a cigarette']
+lineTwo = ['A time of joy, peace and love', 'In pink princess dress and shoes', 'By the way, kitty winks too', 'But not as friendly as dogs', 'Thirteen in arithmetic']
+lineThree = ['The wind sings to me', 'Seems far away', 'I think you are right', 'Right across the street', 'I made a mistake', 'Winter and summer']
+print(lineOne[random.randint(0,3)])
+print(lineTwo[random.randint(0,3)])
+print(lineThree[random.randint(0,3)])
