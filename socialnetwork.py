c089def399008d0cc91d1668924f3103fdeabf46
anweenie <awburns01@gmail.com>
2017-08-10 14:22:23 -0400

    Update socialnetwork.py

--- socialnetwork.py
+++ socialnetwork.py
@@ -1,3 +1,6 @@
+import turtle
+import math
+
 class User:
     # Define the fields and methods for your object here.
     def __init__(self, name):
@@ -22,8 +25,31 @@ class User:
 
 class Network:
     # Define the fields and methods for your object here.
-    def users(self, list):
-        list = []
+    def __init__(self):
+        self.connection = []
+        self.users = []
+        
+    def connection(self):
+        self.connections = []
+
+    def getusers(self):
+        return self.users 
+
+    def add_user(self, alloweduser):
+        self.user.append(alloweduser)
+
+    def addconnection(self, user1, user2):
+        if user1.connection not in connections:
+            user1.addconnection(user2)
+            user2.addconnecton(user1)
+        if user2.connection not in connections:
+            user1.addconnection(user2)
+            user2.addconnection(user1)
+
 
 def main():
     # Define the program flow for your user interface here.
+
+# Runs your program.
+if __name__ == '__main__':
+    main():
